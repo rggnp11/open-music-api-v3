98dfcca9cc02b77fdf3ba611075c1c2c19c04b96
h2yhm <heri2000@yahoo.com>
2023-08-03 23:55:16 +0700

    Create likes table

--- migrations/1691080012057_create-table-likes.js
+++ migrations/1691080012057_create-table-likes.js
@@ -1,12 +1,12 @@
 exports.up = (pgm) => {
   pgm.createTable(
-    'collaborations',
+    'likes',
     {
       id: {
         type: 'VARCHAR(50)',
         primaryKey: true,
       },
-      playlist_id: {
+      album_id: {
         type: 'VARCHAR(50)',
         notNull: true,
       },
@@ -26,28 +26,39 @@ exports.up = (pgm) => {
   );
 
   pgm.addConstraint(
-    'collaborations',
-    'fk_collaborations.playlist_id_playlists.id',
-    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE',
+    'likes',
+    'fk_likes.album_id_albums.id',
+    'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE',
   );
 
   pgm.addConstraint(
-    'collaborations',
-    'fk_collaborations.user_id_users.id',
+    'likes',
+    'fk_likes.user_id_users.id',
     'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE',
+  );
+
+  pgm.addConstraint(
+    'likes',
+    'uk_likes.album_id_likes.user_id',
+    'UNIQUE (album_id, user_id)',
   );
 };
 
 exports.down = (pgm) => {
-  pgm.dropConstraint(
-    'collaborations',
-    'fk_collaborations.user_id_users.id',
+  pgm.dropconstraint(
+    'likes',
+    'uk_likes.album_id_likes.user_id',
   );
 
   pgm.dropConstraint(
-    'collaborations',
-    'fk_collaborations.playlist_id_playlists.id',
+    'likes',
+    'fk_likes.user_id_users.id',
   );
 
-  pgm.dropTable('collaborations');
+  pgm.dropConstraint(
+    'likes',
+    'fk_likes.album_id_albums.id',
+  );
+
+  pgm.dropTable('likes');
 };
